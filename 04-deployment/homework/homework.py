627ba1023762ef6eef1815203e8e007463d0ac5f
Nhật Thanh Văn <94627071+thanhnhatt29@users.noreply.github.com>
2024-06-21 13:29:23 +0000

    homework module 05-monitoring

--- 04-deployment/homework/homework.py
+++ 04-deployment/homework/homework.py
@@ -42,24 +42,10 @@ df = read_data(input_file)
 print(f'processing data...')
 dicts = df[categorical].to_dict(orient='records')
 X_val = dv.transform(dicts)
+
+print(f'predicting...')
 y_pred = model.predict(X_val)
 
 print(f'calculating mean prediction...')
 mean_pred = np.mean(y_pred)
 print(mean_pred)
-# df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
-
-# df_result = pd.DataFrame()
-# df_result['ride_id'] = df['ride_id']
-# df_result['predicted_duration'] = y_pred
-
-
-
-# df_result.to_parquet(
-#     output_file,
-#     engine='pyarrow',
-#     compression=None,
-#     index=False
-# )
-
-# os.path.getsize(f'./output/{taxi_type}_{year:04d}-{month:02d}.parquet')
